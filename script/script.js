b56abd5890dff5a3520419ed40770f06072cd32f
emma <dlaudtjs400@naver.com>
2023-03-17 18:09:43 +0900

    0317

--- script/script.js
+++ script/script.js
@@ -93,3 +93,18 @@ $(document).ready(function(){
     $("#footer").load("/load/footer.html");
 
 });
+
+
+
+// scroll
+jQuery(document).ready(function($){
+	
+	$('a.scroll-link').click(function(e){
+		e.preventDefault();
+		$id = $(this).attr('href');
+		$('body,html').animate({
+			scrollTop: $($id).offset().top -20
+		}, 750);
+	});
+	
+});
